b8fe375341d914ae550286e06f314f115a638d17
Navinkumar1999 <navinkumaradam01@gmail.com>
2023-12-24 11:58:59 +0530

    grid of layout

---
+++ qtify/src/components/Card/Card.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import styles from "./Card.module.css";
+import {Tooltip} from "@mui/material";
+
+function Card({data ,type}){
+
+    const getCard =(type)=>{
+        switch(type){
+            case "album":{
+                const {image,title,follows,songs, slug} =data;
+                return(
+                    <Tooltip title={`${songs.length} songs`} placement ="top" arrow>
+                         <a href={`/album/${slug}`}> 
+                            <div className={styles.wrapper}>
+                                 <div className={styles.card}>
+                                     <img src={image} alt="song" />
+                                    <div className={styles.banner}>
+                                            <div className={styles.fill}>
+                                                <p>{follows} Follows</p> 
+                                            </div>
+                                    </div>
+                                  </div>
+                                <div className={styles.titlewrapper}>
+                                    <p>{title}</p>
+                                 </div>
+                            </div>
+                         </a> 
+                    </Tooltip>
+                )
+                
+                
+    
+            }
+            case "song":{
+                const {image,likes,title} =data;
+                return(
+                <div className={styles.wrapper}>
+                    <div className={styles.card}>
+                        <img src={image} alt="song" />
+                        <div className={styles.banner}>
+                            <div className={styles.fill}>
+                               <p>{likes} Likes</p> 
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.titlewrapper}>
+                        <p>{title}</p>
+                    </div>
+                </div>
+                )
+                
+                
+            }
+            default:{
+                return<></>
+            }
+        }
+    }
+
+    return getCard(type);
+}
+
+
+export default Card;
